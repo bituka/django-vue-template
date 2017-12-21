37a32565a97c5423cdc5e22f57d2935de148c627
Admin <icorrea@tavanoteam.com>
2017-12-21 17:31:53 -0300

    General adjustments

--- Modules/customizations/Theme_Home@Dev/JavaScript/Theme_Home.js
+++ Modules/customizations/Theme_Home@Dev/JavaScript/Theme_Home.js
@@ -1,37 +1,37 @@
 define('Theme_Home', ['Home.View', 'jQuery', 'underscore'], function Theme_Home(HomeView, jQuery, _) {
-    
+
         _.extend(HomeView.prototype, {
             initialize: function () {
                 var self = this;
                 this.windowWidth = jQuery(window).width();
-    
+
                 this.on('afterViewRender', function () {
                     this.listenToOnce(
                         typeof CMS !== 'undefined' ? CMS : Backbone.Events, 'page:content:set', this.initSliders
                     );
                 });
-    
+
                 var windowResizeHandler = _.throttle(function () {
                     if (_.getDeviceType(self.windowWidth) === _.getDeviceType(jQuery(window).width())) {
                         return;
                     }
                     this.showContent();
-    
+
                     _.resetViewportWidth();
-    
+
                     self.windowWidth = jQuery(window).width();
-    
+
                 }, 1000);
-    
+
                 this._windowResizeHandler = _.bind(windowResizeHandler, this);
-    
+
                 jQuery(window).on('resize', this._windowResizeHandler);
-    
+
             }
             , initSliders: function () {
-    
-                var self = this;           
-    
+
+                var self = this;
+
                 _.initBxSlider(self.$('[data-slider]'), {
                     nextText: '<a class="home-gallery-next-icon"></a>'
                     , prevText: '<a class="home-gallery-prev-icon"></a>'
@@ -39,11 +39,11 @@ define('Theme_Home', ['Home.View', 'jQuery', 'underscore'], function Theme_Home(
                     , pause: 6000
                     , pagerCustom: '.main-slider-pager'
                 });
-    
+
                 if(SC.isPageGenerator()){
                     return;
                 }
-    
+
                 var existCondition = setInterval(function() { // We need to wait until element exist
                     if (self.$('.home-items-carousel-container').find('ul.home-merch').length) {
                         clearInterval(existCondition);
@@ -65,7 +65,7 @@ define('Theme_Home', ['Home.View', 'jQuery', 'underscore'], function Theme_Home(
                         });
                     }
                 }, 100);
-    
+
                 // _.delay(function () {
                 //     if (self.$('.home-items-carousel-container').find('ul.home-merch').length) {
                 //         _.initBxSlider(self.$('.home-items-carousel-container').find('ul.home-merch'), {
@@ -81,9 +81,9 @@ define('Theme_Home', ['Home.View', 'jQuery', 'underscore'], function Theme_Home(
                 //         });
                 //     }
                 // },2000);
-    
+
             }
-    
+
         })
-    
+
     });
