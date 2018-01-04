2e9777691cf17f21ce6976e7e8a8e173b9d63a00
Matias Lugli <mlugli@tavanoteam.com>
2018-01-04 10:37:55 -0300

    CMS ON Secure add jquery as dependency

--- Modules/basic_modules/CMSOnSecure@2.0.0/JavaScript/CMSOnSecure.js
+++ Modules/basic_modules/CMSOnSecure@2.0.0/JavaScript/CMSOnSecure.js
@@ -1,6 +1,7 @@
 //NASTY FIX FOR CMS ON SECURE
-define('CMSOnSecure',function(){
-    jQuery(document).ajaxSend(function( event, request, settings ) {
+define('CMSOnSecure',['jQuery'],
+    function(jQuery){
+        jQuery(document).ajaxSend(function( event, request, settings ) {
         if(settings.url.indexOf('/api/cms/session/domain') > -1 && location.href.indexOf('checkout') > -1){
             settings.url = '/c.TSTDRV1391279/automation/dev/services/CMSOnSecure.Service.ss';
         }
