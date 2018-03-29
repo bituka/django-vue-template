6735e5647df91615e6fdda8ef6a853d9340639f2
Gonzalo Abellá <gonzaloabella@Tavanos-MacBook-Pro-3.local>
2018-03-29 14:40:07 -0300

    Facet: Price for package item.

--- Modules/customizations/Theme_ProductViews@Dev/JavaScript/ProductViews.Price.Extension.View.js
+++ Modules/customizations/Theme_ProductViews@Dev/JavaScript/ProductViews.Price.Extension.View.js
@@ -19,11 +19,28 @@ define(
         ProductViewsPriceView.prototype.installPlugin('postContext', {
             priority: 1,
             execute: function execute(context, view) {
-            
+
+              var isNonInv = false;
+              var item = view.model.get('item');
+              if(item != undefined){
+                if (item.get('itemtype') == "NonInvtPart"){
+                  isNonInv = true;
+                  var price = item.get('custitem_tt_price_total');
+                  price = parseFloat(price).toFixed(2);
+                  price = String(price);
+                  // '.50' correction
+                  if (price.substring(price.indexOf("."),price.length) == ".5") {
+                    price = price + "0";
+                  }
+                  price = "USD $" + price;
+                }
+              }
 
       				_.extend(context, {
-                  // @property {string} unitOfMeasure
-                  // unitOfMeasure: view.model.get('item').get('saleunit')
+                  // @property {string} nonInvPrice
+                  nonInvPrice: price ,
+                  // @property {boolean} isNonInv
+                  isNonInv: isNonInv
       				});
             }
         });
