c154427475907a2cf465673e5b953f76544c5439
Nico <ngrimonpont@tavanoteam.com>
2018-05-15 17:37:52 -0300

    fix http error

--- Modules/customizations/Theme_ProductDetails@Dev/JavaScript/ProductDetails.ImageGallery.View.Extension.js
+++ Modules/customizations/Theme_ProductDetails@Dev/JavaScript/ProductDetails.ImageGallery.View.Extension.js
@@ -7,172 +7,141 @@
 
 // @module ProductDetails
 define(
-	'ProductDetails.ImageGallery.View.Extension'
-,	[
-		'ProductDetails.ImageGallery.View'
-	,	'Backbone.CompositeView'
-	,	'Utilities.ResizeImage'
-	,	'SocialSharing.Flyout.Hover.View'
+    'ProductDetails.ImageGallery.View.Extension', [
+        'ProductDetails.ImageGallery.View', 'Backbone.CompositeView', 'Utilities.ResizeImage', 'SocialSharing.Flyout.Hover.View'
 
-	,	'product_details_image_gallery.tpl'
+        , 'product_details_image_gallery.tpl'
 
-	,	'Backbone'
-	,	'underscore'
-	,	'Utils'
+        , 'Backbone', 'underscore', 'Utils'
 
-	,	'jquery.zoom'
-	,	'jQuery.fancybox'
-	,	'jQuery.bxSlider'
-	]
-,	function (
-		ProductDetailsImageGalleryView
-	,	BackboneCompositeView
-	,	resizeImage
-	,	SocialSharingFlyoutHoverView
+        , 'jquery.zoom', 'jQuery.fancybox', 'jQuery.bxSlider'
+    ],
+    function (
+        ProductDetailsImageGalleryView, BackboneCompositeView, resizeImage, SocialSharingFlyoutHoverView
 
-	,	product_details_image_gallery_tpl
+        , product_details_image_gallery_tpl
 
-	,	Backbone
-	,	_
-	,	Utils
-	)
-{
-	'use strict';
-	ProductDetailsImageGalleryView.prototype.installPlugin('postContext', {
-			priority: 1,
-			execute: function execute(context, view) {
-			var model = view.model;
-			// START inclusion of Badges Logic
-			var customVideo = model.get('item').get('custitem_field_video');
-			_.extend(context, {
-						customVideo: customVideo
-				});
-			}
-	});
+        , Backbone, _, Utils
+    ) {
+        'use strict';
+        ProductDetailsImageGalleryView.prototype.installPlugin('postContext', {
+            priority: 1,
+            execute: function execute(context, view) {
+                var model = view.model;
+                // START inclusion of Badges Logic
+                var customVideo = model.get('item').get('custitem_field_video');
+                _.extend(context, {
+                    customVideo: customVideo
+                });
+            }
+        });
 
-	_.extend(ProductDetailsImageGalleryView.prototype, {
+        _.extend(ProductDetailsImageGalleryView.prototype, {
 
-		initialize: function initialize ()
-			{
-				Backbone.View.prototype.initialize.apply(this, arguments);
-				BackboneCompositeView.add(this);
+            initialize: function initialize() {
+                Backbone.View.prototype.initialize.apply(this, arguments);
+                BackboneCompositeView.add(this);
 
-				var self = this;
+                var self = this;
 
-				this.images = this.model.getImages();
+                this.images = this.model.getImages();
 
-				this.model.on('change', function ()
-				{
-					var model_images = this.model.getImages();
-					if (!_.isEqual(this.images, model_images))
-					{
-						this.images = model_images;
-						this.render();
-					}
-				}, this);
+                this.model.on('change', function () {
+                    var model_images = this.model.getImages();
+                    if (!_.isEqual(this.images, model_images)) {
+                        this.images = model_images;
+                        this.render();
+                    }
+                }, this);
 
 
 
-				this.on('afterViewRender', function ()
-				{
-					self.initSlider();
-					self.initZoom();
-					_.defer(self.appendVideo());
-				});
-			}
-			, appendVideo: function(){
-				var customVideo = this.$('#custom-video')
-				this.$('.bx-has-controls-direction').append(customVideo)
-			}
+                this.on('afterViewRender', function () {
+                    self.initSlider();
+                    self.initZoom();
+                    _.defer(self.appendVideo());
+                });
+            },
+            appendVideo: function () {
+                    var customVideo = this.$('#custom-video')
+                    this.$('.bx-has-controls-direction').append(customVideo)
+                }
 
-		, initSlider: function initSlider ()
-			{
+                ,
+            initSlider: function initSlider() {
 
-				var self = this;
+                    var self = this;
 
-				$('.facets-item-cell-grid-quick-view-link, .facets-item-cell-table-quick-view-link, .facets-item-cell-list-quick-view-link').click(function(){
-					_.delay(function () {
-						$('[data-slider]').bxSlider().goToSlide(1);
-					},300);
-				})
+                    $('.facets-item-cell-grid-quick-view-link, .facets-item-cell-table-quick-view-link, .facets-item-cell-list-quick-view-link').click(function () {
+                        _.delay(function () {
+                            $('[data-slider]').bxSlider().goToSlide(1);
+                        }, 300);
+                    })
 
-				if (self.images.length > 1)
-				{
-					self.$slider = Utils.initBxSlider(self.$('[data-slider]'), {
-							buildPager: _.bind(self.buildSliderPager, self)
-						,	startSlide: 0
-						,	adaptiveHeight: true
-						,	touchEnabled: true
-						,	nextText: '<a class="product-details-image-gallery-next-icon" data-action="next-image"></a>'
-						,	prevText: '<a class="product-details-image-gallery-prev-icon" data-action="prev-image"></a>'
-						, 	controls: true
-					});
+                    if (self.images.length > 1) {
+                        self.$slider = Utils.initBxSlider(self.$('[data-slider]'), {
+                            buildPager: _.bind(self.buildSliderPager, self),
+                            startSlide: 0,
+                            adaptiveHeight: true,
+                            touchEnabled: true,
+                            nextText: '<a class="product-details-image-gallery-next-icon" data-action="next-image"></a>',
+                            prevText: '<a class="product-details-image-gallery-prev-icon" data-action="prev-image"></a>',
+                            controls: true
+                        });
 
-					// self.$('[data-action="next-image"]').off();
-					// self.$('[data-action="prev-image"]').off();
+                        // self.$('[data-action="next-image"]').off();
+                        // self.$('[data-action="prev-image"]').off();
 
-					self.$('[data-action="next-image"]').click(_.bind(self.nextImageEventHandler, self));
-					self.$('[data-action="prev-image"]').click(_.bind(self.previousImageEventHandler, self));
-				}
-			}
+                        self.$('[data-action="next-image"]').click(_.bind(self.nextImageEventHandler, self));
+                        self.$('[data-action="prev-image"]').click(_.bind(self.previousImageEventHandler, self));
+                    }
+                }
 
-			,	initZoom: function ()
-			{
-				if (!SC.ENVIRONMENT.isTouchEnabled)
-				{
-					var images = this.images
-					,	self = this;
+                ,
+            initZoom: function () {
+                if (!SC.ENVIRONMENT.isTouchEnabled) {
+                    var images = this.images,
+                        self = this;
 
-					if (this.parentView.template.Name === "product_details_full") {
-						// This is for the fancybox
-						var $links = this.$('[data-fancy]');
-$('.bx-viewport').append('<div>testest</div>')
-						$links.on('click', function(e){
-							var objs = [];
+                    if (this.parentView.template.Name === "product_details_full") {
+                        // This is for the fancybox
+                        var $links = this.$('[data-fancy]');
+                        $('.bx-viewport').append('<div>testest</div>')
+                        $links.on('click', function (e) {
+                            var objs = [];
 
-							for(var i = 0; i < $links.length; i++) {
-								var link = $links[i];
+                            for (var i = 0; i < $links.length; i++) {
+                                var link = $links[i];
 
-								var obj = {
-									src : self.$(link).data('fancy'),
-									type : 'image'
-								}
+                                var obj = {
+                                    src: self.$(link).data('fancy'),
+                                    type: 'image'
+                                }
 
-								objs.push(obj);
-							}
+                                objs.push(obj);
+                            }
+                            $.fancybox.open(objs);
 
+                            return false;
+                        });
+                        // fancy box functioanlity
+                    } else {
+                        this.$('[data-zoom]').each(function (slide_index) {
+                            self.$(this).zoom({
+                                url: resizeImage(images[slide_index].url, 'zoom'),
+                                callback: function () {
+                                    var $this = self.$(this);
 
-							$.fancybox.open( objs);
+                                    if ($this.width() <= $this.closest('[data-view="Product.ImageGallery"]').width()) {
+                                        $this.remove();
+                                    }
 
-							return false;
-						});
-						// fancy box functioanlity
-					} else {
-						this.$('[data-zoom]').each(function (slide_index)
-						{
-							self.$(this).zoom({
-								url: resizeImage(images[slide_index].url, 'zoom')
-							,	callback: function()
-								{
-									var $this = self.$(this);
-
-									if ($this.width() <= $this.closest('[data-view="Product.ImageGallery"]').width())
-									{
-										$this.remove();
-									}
-
-									return this;
-								}
-							});
-						});
-					}
-
-
-
-				}
-			}
-
-  });
-
-
-});
+                                    return this;
+                                }
+                            });
+                        });
+                    }
+                }
+            }
+        });
+    });
