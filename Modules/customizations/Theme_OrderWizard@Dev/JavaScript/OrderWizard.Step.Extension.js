f25c57f9c014ba2e775372a954c3840534badf2c
Carlos Rodriguez <crodriguez@tavanoteam.com>
2018-05-30 15:13:02 -0300

    Fix cc being lost in review step

--- Modules/customizations/Theme_OrderWizard@Dev/JavaScript/OrderWizard.Step.Extension.js
+++ Modules/customizations/Theme_OrderWizard@Dev/JavaScript/OrderWizard.Step.Extension.js
@@ -19,37 +19,44 @@ define(
 
         _.extend(OrderWizardStep.prototype, {
 
-    			stepAdvance: function(e){
-
+          present: function ()
+          {
             /* Start - Customization: change currency from shipping address */
       			var url = window.location.href;
-      			if(url.indexOf('#review') != -1) {
-      				var liveOrderModel = LiveOrderModel.getInstance();
-              var addresses = liveOrderModel.get('addresses').models;
-              var country = '';
-              for(var i=0; i<addresses.length; i++){
-                if(addresses[i].attributes.defaultshipping == "T"){
-                  country = addresses[i].attributes.country;
+      			if (url.indexOf('#review') != -1 && LiveOrderModel.loadCart().state() === 'resolved') {
+
+              LiveOrderModel.loadCart().done(function () {
+
+                var liveOrderModel = LiveOrderModel.getInstance();
+                
+
+                var shippingAddress = liveOrderModel.get('addresses').findWhere({defaultshipping: 'T'});
+                var country = shippingAddress && shippingAddress.get('country');
+
+                var urlRedirect = url.substring(0,url.indexOf('&n=2'));
+                if(country == "CA") {
+                  urlRedirect = urlRedirect + "&n=2&cur=CAD#review";                  
+                }else{
+                  urlRedirect = urlRedirect + "&n=2&cur=USD#review";
                 }
-              }
-              var urlRedirect = url.substring(0,url.indexOf('&n=2'));
-              if(country == "CA") {
-                var urlDef = urlRedirect + "&n=2&cur=CAD#review";
-                window.location.href = urlDef;
-              }else{
-                var urlDef = urlRedirect + "&n=2&cur=USD#review";
-      					window.location.href = urlDef;
-      				}
-      			}
+
+                if (urlRedirect != url)
+                {
+                  _.defer(function () {
+                    console.log('Redirecting...');                  
+                    window.location.href = urlRedirect;
+                  });
+                }               
+
+              });      				
+            }
+
+            else {
+              //console.log('Cart state: ', LiveOrderModel.loadCart().state());              
+            }            
+            
       			/* End - Customization: change currency from shipping address */
-
-      			if (this.areAllModulesReady())
-      			{
-      				return this.isStepReady() || this.wizard.isPaypalComplete();
-      			}
-
-      			return false;
-    			}
+          }
 
       });
 });
